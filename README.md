f9fd914f5db9aad657f9925790c7145a44f7b401
TylerWilliams97 <w.tyler1997@gmail.com>
2020-12-11 22:20:45 -0800

    Added css elements and local starage

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Day-Planner
-Day planner for orginazation
+-Day planner-
+A simple day planner to save what needs to be done during your busy day.
+using the magic of javascript you can now remember all the important things that need to get done!
--- script.js
+++ script.js
@@ -1,9 +1,49 @@
+$(document).ready(function(){
+
 var time = luxon.DateTime.local()
-var date = time.day+"/"+time.month+"/"+time.year;
+var date = time.month+"/"+time.day+"/"+time.year;
 var localTime = time.hour+":"+time.minute;
+var container = $('.container');
 console.log(time);
 console.log(date);
 console.log(localTime);
 
 $("#currentDay").text(date);
 
+function timeFormat(i){
+    if (i === 12){
+        return '12PM'
+    }
+    if (i > 12){
+        return i-12+'PM'
+    }
+
+    return i +'AM'
+}
+
+for (let i = 9; i < 18; i++) {
+    const timeBlock = $("<div>")
+    .attr("id",i)
+    .addClass('row time-block past');
+
+    timeBlock.append($("<div>").addClass('col-md-1 hour').text(timeFormat(i)));
+    timeBlock.append($('<textarea>').addClass('col-md-10 description'));
+    timeBlock.append($('<button>').addClass('btn saveBtn col-md-1')
+    .append('<i>').addClass('fas fa-save'));
+
+
+    container.append(timeBlock);
+}
+$(".saveBtn").on('click', function(){
+    const time = $(this).parent().attr('id');
+    const text = $(this).siblings('.description').val();
+
+    localStorage.setItem(time , text);
+    
+}
+)
+for (let i = 9; i < 18; i++){
+$('#'+i).children().siblings("textarea").val(localStorage.getItem(i));
+}
+
+});
